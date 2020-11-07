be1b6926d4d48f42e16d60c6b7d4d83c57459027
w <w1575@ya.ru>
2020-11-07 17:06:35 +0500

    =init

---
+++ common/components/fixtures/BaseFixture.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace common\components\fixtures;
+
+use yii\test\ActiveFixture;
+
+class BaseFixture extends ActiveFixture
+{
+    public $dataDirectory = "@common/tests/_data/";
+}
---
+++ common/fixtures/UserFixture.php
@@ -0,0 +1,11 @@
+<?php
+namespace common\fixtures;
+
+use common\components\fixtures\BaseFixture;
+
+class UserFixture extends BaseFixture
+{
+    public $modelClass = 'common\models\User';
+
+    // public $dataFile = "@common/tests/_data/user.php";
+}
