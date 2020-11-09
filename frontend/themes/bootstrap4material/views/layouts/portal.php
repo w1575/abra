0499de10a1eef6bd79dcf5990c3da6293fb92bd3
w <w1575@ya.ru>
2020-11-10 00:35:46 +0500

    Portal layout with tabs

---
+++ frontend/themes/bootstrap4material/views/layouts/portal.php
@@ -0,0 +1,25 @@
+<?php $this->beginContent("@frontend/themes/bootstrap4material/views/layouts/main.php") ?>
+<?= \yii\bootstrap4\Nav::widget([
+      'items' => [
+          [
+              'label' => 'Аккаунты',
+              'url' => ['/portal/account'],
+              'active' => $this->context->id === 'portal/account',
+          ],
+          [
+              'label' => 'Порталы',
+              'url' => ['/portal/portal'],
+              'active' => $this->context->id === 'portal/portal',
+          ],
+      ],
+      'options' => ['class' => 'nav nav-tabs']
+])?>
+<div class="tab-content" id="myTabContent">
+    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+        <div class="tab-content-wrap">
+            <?= $content ?>
+        </div>
+    </div>
+</div>
+
+<?php $this->endContent() ?>
