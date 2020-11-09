b73f11c25a580650d5d6fa8b8942039c5f0743e8
w <w1575@ya.ru>
2020-11-10 00:35:29 +0500

    Main controller with some common settings

---
+++ frontend/components/controllers/MainController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace frontend\components\controllers;
+
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+
+class MainController extends \yii\web\Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'actions' => [],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ]
+                ],
+            ],
+        ];
+    }
+}
