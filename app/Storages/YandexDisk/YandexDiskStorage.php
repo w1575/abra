3d1f197147914fd3b94a10f6aa8d790a0b3a93ae
w <dbludev@ya.ru>
2023-07-11 04:48:23 +0500

    Some interfaces settings

--- app/Storages/YandexDisk/YandexDiskStorage.php
+++ app/Storages/YandexDisk/YandexDiskStorage.php
@@ -7,12 +7,9 @@ use App\Data\Storages\Config\YandexDiskConfigData;
 use App\Data\Storages\FileInfo\FileInfoData;
 use App\Data\Storages\Settings\StorageSettingsData;
 use App\Data\Storages\YandexDisk\FilesListResponseData;
-use App\Storages\StorageContract;
-use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\ClientInterface;
 use Psr\Http\Message\ResponseInterface;
-use Spatie\LaravelData\Contracts\DataObject;
 use Spatie\LaravelData\DataCollection;
 
 class YandexDiskStorage implements YandexDiskStorageContract
@@ -25,7 +22,7 @@ class YandexDiskStorage implements YandexDiskStorageContract
         protected ClientInterface $client
     )
     {
-        d
+
     }
 
     protected function getAuthorizationHeader(): array
